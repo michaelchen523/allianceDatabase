b511da1ba3eb0cc4893c30b44d7ea9f9e5aae0aa
Sruti <sruti@gatech.edu>
2016-09-27 15:38:40 -0400

    added login.html

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html', title = 'Alliance')
+
+def login()
+    return render_template('login.html', title='login')
