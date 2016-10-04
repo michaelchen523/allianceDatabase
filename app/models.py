140da31457b16b14a7f26bd980afe1c12bc049a0
sfinch6 <sfinch6@gatech.edu>
2016-10-04 19:51:12 -0400

    started implementing sqlalchemy

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from app import db
 class User(db.Model):
-    username = db.Column(db.String(20), nullable = False, unique=True, primary_key=True)
-    email = db.Column(db.String(30), nullable = False)
-    password = db.Column(db.String(25), nullable = False)
-    name = db.Column(db.String(20), nullable = False)
+    Username = db.Column(db.String(20), primary_key = True)
+    Email = db.Column(db.String(30), nullable = False)
+    Password = db.Column(db.String(25), nullable = False)
+    Name = db.Column(db.String(20), nullable = False)
 
     def __init__(self, username, email, password, name):
         self.username = username
@@ -13,3 +13,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Organization(db.Model):
+    Name = db.Column(db.String(20), primary_key = True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Organization %r>' % self.name
+
+class Resources(db.Model):
+    AdminName = db.Column(db.String(20), nullable = False)
+    Name = db.Column(db.String(20), )
