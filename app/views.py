5145d29b4f5f72836b37389cc543f8f479731068
michaelchen523 <michaelchen523@gmail.com>
2016-10-03 20:39:50 -0400

    Temp functionality

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, url_for, redirect, flash
+from .models import User
+
 @app.route('/')
 def index():
     return render_template('index.html', title = 'Alliance')
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html', title='login')
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        registered_user = User.query.filter_by(username=username,password=password).first()
+        if registered_user is None:
+            flash('Username or Password is invalid', 'error')
+            return redirect(url_for('login'))
+
+    return render_template('login.html', title='login', error=error)
