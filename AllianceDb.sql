54fa587d1cb87f300473e1a8689ba53ae51fd213
sfinch6 <sfinch6@gatech.edu>
2016-10-11 19:15:20 -0400

    stuff

---
+++ AllianceDb.sql
@@ -0,0 +1,125 @@
+DROP TABLE IF EXISTS `Organization`;
+
+CREATE TABLE `Organization`
+ (`Name` VARCHAR(20),
+  PRIMARY KEY (`Name`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `User`;
+
+CREATE TABLE `User`(
+ `Username` VARCHAR(20)  NOT NULL,
+ `Email` VARCHAR(30) NOT NULL,
+ `Password` VARCHAR(25) NOT NULL,
+ `Name` VARCHAR(20) NOT NULL,
+ `Organization` VARCHAR(20) NOT NULL,
+ `Admin` INT NOT NULL,
+ PRIMARY KEY (`Username`),
+ FOREIGN KEY (`Organization`) REFERENCES `Organization` (`Name`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Resource`;
+
+CREATE TABLE `Resource`
+ (`AdminName` VARCHAR(20)  NOT NULL,
+  `Name` VARCHAR(20) NOT NULL,
+  `Username` VARCHAR(20) NOT NULL,
+  `Address_State` VARCHAR(2) NOT NULL,
+  `Address_City` VARCHAR(20) NOT NULL,
+  `Address_Zip` INT NOT NULL,
+  `Address_Street` VARCHAR(20) NOT NULL,
+  `Address_Number` INT NOT NULL,
+  `Description` VARCHAR(600),
+  `ID` MEDIUMINT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`),
+ FOREIGN KEY (`Username`) REFERENCES `User` (`Username`),
+ CONSTRAINT `Duplicate_Resource` UNIQUE (`Name`, `Address_State`, `Address_City`, `Address_Zip`, `Address_Street`, `Address_Number`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `User_Favorites`;
+
+CREATE TABLE `User_Favorites` (
+  `Username` VARCHAR(20)  NOT NULL,
+  `ID` MEDIUMINT NOT NULL,
+  PRIMARY KEY (`Username`, `ID`),
+  FOREIGN KEY (`Username`) REFERENCES `User` (`Username`),
+  FOREIGN KEY (`ID`) REFERENCES `Resource` (`ID`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Org_Favorites`;
+
+CREATE TABLE `Org_Favorites`
+ (`Organization` VARCHAR(20)  NOT NULL,
+  `ID` MEDIUMINT NOT NULL,
+  PRIMARY KEY (`Organization`, `ID`),
+  FOREIGN KEY (`Organization`) REFERENCES `Organization` (`Name`),
+  FOREIGN KEY (`ID`) REFERENCES `Resource`(`ID`));
+
+
+DROP TABLE IF EXISTS `Number`;
+
+CREATE TABLE `Number`
+  (`PhoneNumber` INT NOT NULL,
+  `Name` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`PhoneNumber`, `Name`),
+ FOREIGN KEY (`Name`) REFERENCES `Resource` (`Name`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Food`;
+
+CREATE TABLE `Food`
+  (`ID` MEDIUMINT NOT NULL,
+  `Avg_Cost` INT,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`ID`) REFERENCES `Resource` (`ID`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Medical`;
+
+CREATE TABLE `Medical`
+  (`ID` MEDIUMINT NOT NULL,
+  `Insurance` VARCHAR(20),
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`ID`) REFERENCES `Resource` (`ID`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Religious`;
+
+CREATE TABLE `Religious`
+  (`ID` MEDIUMINT NOT NULL,
+  `Denomonation` VARCHAR(20),
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`ID`) REFERENCES `Resource` (`ID`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Shelter`;
+
+CREATE TABLE Shelter
+  (`ID` MEDIUMINT NOT NULL,
+  `Capacity` INT,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`ID`) REFERENCES `Resource` (ID)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Reviews`;
+
+CREATE TABLE Reviews
+  (`ID` MEDIUMINT NOT NULL,
+   `Username` VARCHAR(20) NOT NULL,
+   `Score` INT NOT NULL,
+   `Description` VARCHAR(600),
+   PRIMARY KEY (`ID`, `Username`),
+   FOREIGN KEY (`ID`) REFERENCES `Resource` (`ID`),
+   FOREIGN KEY (`Username`) REFERENCES `User` (`Username`)
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
