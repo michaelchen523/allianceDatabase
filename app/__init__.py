d5b0a31951a931ac93cf7301726f41a400df46e6
michaelchen523 <michaelchen523@gmail.com>
2016-10-13 00:18:22 -0400

    Universal sql code

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
 app.config['MYSQL_USER'] = ''
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = ''
 app.config['MYSQL_HOST'] = ''
+
 app.secret_key= "secretKey1"
 
 mysql = MySQL(app)
