237dfa8bb2c9b215dc1e21289ea9b137f240215d
Sruti Guhathakurta <sruti@gatech.edu>
2016-10-24 23:03:46 -0400

    un-hardcoded some stuff

--- app/views.py
+++ app/views.py
@@ -23,34 +23,96 @@ def login():
 
         if len(data) > 0:
             session['logged_in'] = True
-            return render_template("home.html", title='home', user = username);
+            session['user'] = username
+            session['categories'] = [["Housing", "Housing Description"], ["Medical", "Medical Description"], ["Mental Health", "Mental Health Description"],
+                                     ["Job Readiness", "Job Readiness Description"], ["Legal", "Legal Description"], ["Employment", "Employment Description"],
+                                     ["Transportation", "Transportation Description"], ["Professional Mentors", "Professional Mentors Description"],
+                                     ["Childcare", "Childcare Description"], ["Vehicle", "Vehicle Description"], ["Life Skills", "Life Skills Description"]
+                                        , ["Survivor Network", "Survivor Network Description"]]
+            '''conn = mysql.connection
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM Categories;")
+            session['categories'] = cursor.fetchall()'''
 
+            return redirect(url_for('home'))
     return render_template("login.html")
 
 @app.route('/home')
 def home():
-    return render_template('home.html', title='home')
+    user = session.get('user')
+    categories = session.get('categories')
+    return render_template('home.html', title='home', user = user, categories = categories)
 
-@app.route('/edit_user')
+@app.route('/edit_user', methods = ['GET', 'POST'])
 def edit_user():
-    return render_template('edit_user.html', title = 'profile')
+    user = session.get('user')
+    categories = session.get('categories')
+    conn = mysql.connection
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM USER WHERE Username='" + user + "';")
+    userdata = cursor.fetchall()
+    cursor2 = conn.cursor()
+    cursor2.execute("SELECT Name FROM RESOURCE WHERE Username='" + user + "';")
+    userresource = cursor2.fetchall()
+    if request.method == 'POST':
+        orgName = request.form['orgName']
+        orgPhone = request.form['orgPhone']
+        orgDescription = request.form['orgDescription']
+
+        '''cursor.execute("UPDATE Users SET Organization = '" + orgName + "', Phone = '" + orgPhone +
+                       "', Description = '" + orgDescription + "' WHERE User = '" + user + "';")'''
+
+    return render_template('edit_user.html', title = 'edit profile', user = user, categories = categories, userdata = userdata, userresource = userresource)
 
 @app.route('/search')
 def search():
-    return render_template('search.html', title='search')
+    user = session.get('user')
+    categories = session.get('categories')
+    if 'searchcategory' in request.args:
+        searchcategory = request.args['searchcategory']
+        conn = mysql.connection
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM RESOURCE WHERE Category = '" + searchcategory + "';")
+    return render_template('search.html', title='search', user = user, categories = categories, searchcategory = searchcategory)
 
 @app.route('/resource_detail')
 def resource_detail():
-    return render_template('resource_detail.html', title='Resource Details')
+    user = session.get('user')
+    '''resource = request.args['resourcename']
+    conn = mysql.connection
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM RESOURCE WHERE Name = '" + resource + "';")'''
+    categories = session.get('categories')
+    return render_template('resource_detail.html', title='resource details',
+                           user = user, categories = categories)
 
 @app.route('/organizations')
 def organizations():
-    return render_template('organizations.html', title='Organizations')
+    user = session.get('user')
+    categories = session.get('categories')
+    conn = mysql.connection
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Organization;")
+    orgdata = cursor.fetchall()
+    return render_template('organizations.html', title='Organizations', user = user,
+                           orgdata = orgdata, categories = categories)
 
 @app.route('/user_detail')
 def user_detail():
-    return render_template('user_detail.html', title='User Details')
+    user = session.get('user')
+    categories = session.get('categories')
+    detailorg = False
+    if 'detailorg' in request.args:
+        detailorg = request.args['detailorg']
+        conn = mysql.connection
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Organization WHERE Name = '" + detailorg + "';")
+        orgdata = cursor.fetchall
+    return render_template('user_detail.html', title='User Details', user = user,
+                           orgdata = orgdata, detailorg = detailorg, categories = categories)
 
 @app.route('/edit_add_resource')
 def edit_add_resource():
-    return render_template('edit_add_resource.html', title = "Edit Resource")
+    user = session.get('user')
+    categories = session.get('categories')
+    return render_template('edit_add_resource.html', title = "Edit Resource", user = user, categories = categories)
