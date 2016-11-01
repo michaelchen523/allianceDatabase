34cb8b05cfee3429402cf77911a65850a0bfe05f
michaelchen523 <michaelchen523@gmail.com>
2016-11-01 19:35:59 -0400

    Universal server credentials

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,6 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'alliance'
-app.config['MYSQL_HOST'] = '127.0.0.1'
-
 app.secret_key= "secretKey1"
 
 mysql = MySQL(app)
