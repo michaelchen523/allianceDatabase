f65de522082020d5facdc2f643d56222853340ee
Sruti Guhathakurta <sruti@gatech.edu>
2016-11-15 16:10:50 -0500

    all the front-end resource stuff works...

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -31,45 +31,73 @@ $(document).ready(function() {
         $("#cancel").hide();
         if($("#Childcare").is(':checked')) {
             $(".Childcare").show();
+        } else {
+            $(".Childcare").hide();
         }
         if($("#Education").is(':checked')) {
             $(".Education").show();
+        } else {
+            $(".Education").hide();
         }
         if($("#Employment").is(':checked')) {
             $(".Employment").show();
+        } else {
+            $(".Employment").hide();
         }
         if($("#Housing").is(':checked')) {
             $(".Housing").show();
+        } else {
+            $(".Housing").hide();
         }
         if($("#Job_Readiness").is(':checked')) {
             $(".Job_Readiness").show();
+        } else {
+            $(".Job_Readiness").hide();
         }
         if($("#Legal").is(':checked')) {
             $(".Legal").show();
+        } else {
+            $(".Legal").hide();
         }
         if($("#Life_Skills").is(':checked')) {
             $(".Life_Skills").show();
+        } else {
+            $(".Life_Skills").hide();
         }
         if($("#Medical").is(':checked')) {
             $(".Medical").show();
+        } else {
+            $(".Medical").hide();
         }
         if($("#Mental_Health").is(':checked')) {
             $(".Mental_Health").show();
+        } else {
+            $(".Mental_Health").hide();
         }
         if($("#Mentors").is(':checked')) {
             $(".Mentors").show();
+        } else {
+            $(".Mentors").hide();
         }
         if($("#Networks").is(':checked')) {
             $(".Networks").show();
+        } else {
+            $(".Networks").hide();
         }
         if($("#Supplies").is(':checked')) {
             $(".Supplies").show();
+        } else {
+            $(".Supplies").hide();
         }
         if($("#Transportation").is(':checked')) {
             $(".Transportation").show();
+        } else {
+            $(".Transportation").hide();
         }
         if($("#Vehicle").is(':checked')) {
             $(".Vehicle").show();
+        } else {
+            $(".Vehicle").hide();
         }
 
     })
