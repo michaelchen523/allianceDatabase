44cf058495480c183f04085baab4662827510d93
michaelchen523 <michaelchen523@gmail.com>
2016-11-14 20:27:02 -0500

    Changes made

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -8,7 +8,7 @@ function goBack() {
     window.history.back();
 }
 
-function searchname() {
-     var sname = document.getElementById("Search").value;
-     window.location = {{ url_for('searchName', name= sname) }};
-}
+// function searchname() {
+//      var sname = document.getElementById("Search").value;
+//      window.location = {{ url_for('searchName', name= sname) }};
+// }
